6860c4167a9fa8ec435cbab13cf767e787198cf6
Rahul Hembrom <69256240+zammyk@users.noreply.github.com>
2023-05-01 20:57:57 +0530

    Add player object

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-const Gameboard = ((document) => {
+const Player = (name, symbol) => {
+  return { name, symbol };
+};
+
+const Game = ((document) => {
+  let gameboard = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+
+  const p1 = Player("Rahul", "X");
+  const p2 = Player("Caro", "O");
+
+  let currPlayer = p1;
   const gameboard_container = document.getElementById("gameboard_container");
+
+  const switchCurrPlayer = () => {
+    if (currPlayer === p1) currPlayer = p2;
+    else currPlayer = p1;
+  };
+
+  const playTurn = (cell) => {
+    if (gameboard[cell.row][cell.col] != "") return;
+    gameboard[cell.row][cell.col] = currPlayer.symbol;
+    cell.textContent = currPlayer.symbol;
+  };
 
   (() => {
     for (
@@ -11,16 +36,11 @@ const Gameboard = ((document) => {
         child.row = Math.floor(counter / 3);
         child.col = counter % 3;
         counter++;
-        child.addEventListener("click", function (child) {
-          console.log(this.row, this.col);
+        child.addEventListener("click", () => {
+          playTurn(child);
+          switchCurrPlayer();
         });
       }
     }
   })();
-
-  gameboard = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-  ];
 })(document);
